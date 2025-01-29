27f459d551e3bf37831b234709fd9be6eac87bae
rahmanovdev <jumadilrahmonov@gmail.com>
2025-01-30 01:28:08 +0600

    fg

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import scss from "./HomePage.module.scss";
+import HeroSection from "./home/HeroSection/HeroSection";
+import AboutSection from "./home/AboutSection/AboutSection";
+import TariffsSection from "./home/TariffsSection/TariffsSection";
+import StepsHajjSection from "./home/StepsOfHajjSection/StepsHajjSection";
+import QuestionsSection from "./home/QuestionsSection/QuestionsSection";
+import InformationSection from "./home/InformationSection/InformationSection";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <TariffsSection />
+      <StepsHajjSection />
+      <QuestionsSection />
+      <InformationSection />
+    </>
+  );
 };
 
 export default HomePage;
--- src/components/pages/home/StepsOfHajjSection/StepsHajjSection.tsx
+++ src/components/pages/home/StepsOfHajjSection/StepsHajjSection.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const StepsHajjSection = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Steps to Hajj</h1>
+    </div>
+  );
 };
 
 export default StepsHajjSection;

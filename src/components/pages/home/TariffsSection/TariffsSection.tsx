77736d4561cf8eefb7c6fe3be6737a4bac443a72
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-01-30 17:55:12 +0600

    tarif

--- src/components/pages/home/TariffsSection/TariffsSection.tsx
+++ src/components/pages/home/TariffsSection/TariffsSection.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import scss from "./TariffsSection.module.scss"
+import EkonomCard from "@/components/ui/cards/tours_cards/ekonom_card/EkonomCard";
+import ComfordCard from "@/components/ui/cards/tours_cards/comfort_card/ComfordCard";
+import ComfortPlusCard from "@/components/ui/cards/tours_cards/comfortPlus_card/ComfortPlusCard";
 
 const TariffsSection = () => {
   return <section className={scss.Main}>
     <div className="container">
       <div className={scss.content}>
-        <h1>Tariffs Section</h1>
+        <h1>Тарифы</h1>
+
+        <div className={scss.cards}>
+          <EkonomCard/>
+          <ComfordCard/>
+          <ComfortPlusCard/>
+        </div>
       </div>
     </div>
   </section>;

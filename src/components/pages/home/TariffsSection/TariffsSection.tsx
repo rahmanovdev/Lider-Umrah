2b70f7d65c5217348ff178692ecbe4c576db652f
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-01-30 16:30:27 +0600

    structure

--- src/components/pages/home/TariffsSection/TariffsSection.tsx
+++ src/components/pages/home/TariffsSection/TariffsSection.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import scss from "./TariffsSection.module.scss"
 
 const TariffsSection = () => {
-  return <div></div>;
+  return <section className={scss.Main}>
+    <div className="container">
+      <div className={scss.content}>
+        <h1>Tariffs Section</h1>
+      </div>
+    </div>
+  </section>;
 };
 
-export default TariffsSection;
+export default TariffsSection; 

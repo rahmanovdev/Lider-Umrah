6b9ed591a89aac36cdee92b9c222057320fe2742
rahmanovdev <jumadilrahmonov@gmail.com>
2025-02-10 16:21:35 +0600

    about_program

--- src/components/pages/packages/PackegesDetailContent/PackegesDetailContent.tsx
+++ src/components/pages/packages/PackegesDetailContent/PackegesDetailContent.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import scss from "./PackegesDetailContent.module.scss";
 import HeadPackegeDet from "./Head_PackeckgeDet/HeadPackegeDet";
 import AboutProgramDet from "./AboutProgramDet/AboutProgramDet";
+import About_ProgramDet from "./About_ProgramDet/About_ProgramDet";
 
 const PackegesDetailContent = () => {
   return (
     <>
       <HeadPackegeDet />
       <AboutProgramDet />
+      <About_ProgramDet />
     </>
   );
 };
